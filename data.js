d289e1349456d80c42528158f467cfb2d6ba8c27
Dung Nguyen <dngtiennguyen600@gmail.com>
2019-09-30 17:41:30 +0700

    commit 5

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ var Card = require('./Card');
 var data = [];
 
 for (let index = 0; index < 1000; index++) {
-    var newCard = new Card("Card no " +index + 1, index + 1);
+    var newCard = new Card("Card no " + (index + 1), (Math.round(Math.random()*500) + 1), "https://picsum.photos/id/"+(Math.round(Math.random()*500) + 1)+"/200/300");
     data.push(newCard);
 }
 
